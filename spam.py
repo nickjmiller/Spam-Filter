f8131a4ffab08cfb88352252ec375a3cb28510e5
nickjmiller <nickamilli@gmail.com>
2017-05-10 21:48:56 -0700

    Converted to pandas, implemented desperate memory management

--- spam.py
+++ spam.py
@@ -1,37 +1,46 @@
 import nltk
 import sqlite3
 import random
+import pandas as pd
+import numpy as np
 
 conn = sqlite3.connect('yelpHotelData.db')
-c = conn.cursor()
+query = 'SELECT reviewContent, rating, usefulCount, coolCount, funnyCount FROM review WHERE flagged = "Y" OR flagged = "YR"'
+fake = pd.read_sql(query, conn)
+query = 'SELECT reviewContent, rating, usefulCount, coolCount, funnyCount FROM review WHERE flagged = "N" OR flagged = "NR"'
+real = pd.read_sql(query, conn)
+conn.close()
 
-fake = []
-real = []
-for row in c.execute('SELECT reviewContent, rating, usefulCount, coolCount, funnyCount FROM review WHERE flagged = "Y" OR flagged = "YR" '):
-    fake.append([nltk.word_tokenize(row[0]), row[1], row[2], row[3], row[4],'fake'])
-random.shuffle(fake)
-fake = fake[:500]
 
-for row in c.execute('SELECT reviewContent, rating, usefulCount, coolCount, funnyCount FROM review WHERE flagged = "N" OR flagged = "NR" '):
-    real.append([nltk.word_tokenize(row[0]), row[1], row[2], row[3], row[4],'real'])
-random.shuffle(real)
-real = real[:500]
-documents = real + fake
-random.shuffle(documents)
+fake = fake.iloc[np.random.permutation(5000)]
+fake['tag'] = 'fake'
+real = real.iloc[np.random.permutation(5000)]
+real['tag'] = 'real'
 
-all_words = nltk.FreqDist(word.lower() for (doc,rt,use,cool,fun,tg) in documents for word in doc)
+df = pd.concat([fake,real])
+df = df.iloc[np.random.permutation(len(df))]
+df['reviewContent'] = df.apply(lambda row: nltk.word_tokenize(row['reviewContent']), axis=1)
+
+all_words = nltk.FreqDist(word.lower() for row in df['reviewContent'] for word in row)
 word_features = list(all_words)[:2000]
+del(all_words)
+del(real)
+del(fake)
 
-def document_features(doc,rt,use,cool,fun):
-    document_words = set(doc)
+def document_features(doc):
+    document_words = set(doc['reviewContent'])
     features = {}
     for word in word_features:
         features['contains ' + word] = (word in document_words)
-    features.update({'rating': rt, 'useful': use, 'cool': cool, 'funny': fun, 'length': len(doc)})
-    return features
+    features.update(
+    {'rating': doc['rating'], 'useful': doc['usefulCount'], 'cool': doc['coolCount'], 'funny': doc['funnyCount'], 'length': len(doc['reviewContent'])})
+    return [features,doc['tag']]
 
-featuresets = [(document_features(d,rt,use,cool,fun), c) for (d,rt,use,cool,fun,c) in documents]
-train_set, test_set = featuresets[200:], featuresets[:200]
+featuresets = df.apply(document_features, axis = 1)
+train_set, test_set = featuresets[1000:], featuresets[:1000]
+del(word_features)
+del(featuresets)
+
 classifier = nltk.NaiveBayesClassifier.train(train_set)
 print(nltk.classify.accuracy(classifier, test_set))
 classifier.show_most_informative_features(7)
