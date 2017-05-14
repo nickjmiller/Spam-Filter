117504b359bd8ecba3305611acee34e7b3125538
nickjmiller <nickamilli@gmail.com>
2017-05-13 18:35:51 -0700

    Adding bigrams

--- hotel_spam.py
+++ hotel_spam.py
@@ -23,6 +23,10 @@ df['reviewContent'] = df.apply(lambda row: nltk.word_tokenize(row['reviewContent
 all_words = nltk.FreqDist(word.lower() for row in df['reviewContent'] for word in row)
 word_features = list(all_words)[:2000]
 
+df['bigrams'] = df.apply(lambda row: list(nltk.bigrams(row['reviewContent'])), axis=1)
+bigrams = nltk.FreqDist(word[0].lower() +" "+ word[1].lower() for row in df['bigrams'] for word in row)
+bigrams = list(bigrams)[:500]
+
 del(all_words)
 del(real)
 del(fake)
@@ -30,10 +34,27 @@ del(fake)
 def document_features(doc):
     document_words = set(doc['reviewContent'])
     features = {}
+    # Grabbing the bigrams
+    bigSet = []
+    for word in doc['bigrams']:
+        bigSet.append(word[0].lower() + " " +word[1].lower())
+    bigSet = set(bigSet)
+    for word in bigrams:
+        features['bigram: ' + word] = (word in bigSet)
+
+    # Counting the pronoun usage
+    meCount = 0
+    for word in doc['reviewContent']:
+        if (word.lower() == 'i' or word.lower() == 'me'):
+            meCount += 1
+    me = False
+    if (meCount > 5):
+        me = True
+
     for word in word_features:
         features['contains ' + word] = (word in document_words)
     features.update(
-    {'rating': doc['rating'], 'useful': doc['usefulCount'], 'cool': doc['coolCount'], 'funny': doc['funnyCount'], 'length': len(doc['reviewContent'])})
+    {'rating': doc['rating'], 'useful': doc['usefulCount'], 'cool': doc['coolCount'], 'funny': doc['funnyCount'], 'meCount': me})
     return [features,doc['tag']]
 
 featuresets = df.apply(document_features, axis = 1)
@@ -43,7 +64,7 @@ del(featuresets)
 
 classifier = nltk.NaiveBayesClassifier.train(train_set)
 print(nltk.classify.accuracy(classifier, test_set))
-classifier.show_most_informative_features(7)
+classifier.show_most_informative_features(20)
 
 from sklearn.svm import SVC
 svmClass = SklearnClassifier(SVC(C = .7)).train(train_set)
