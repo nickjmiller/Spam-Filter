19b90f3c96f00c3dac8e58cf6a8ab27965adf7da
nickjmiller <nickamilli@gmail.com>
2017-05-16 17:50:09 -0700

    added svm feature extractor

--- hotel_spam.py
+++ hotel_spam.py
@@ -57,9 +57,9 @@ def document_features(doc):
     {'rating': doc['rating'], 'useful': doc['usefulCount'], 'cool': doc['coolCount'], 'funny': doc['funnyCount'], 'meCount': me})
     return [features,doc['tag']]
 
+
 featuresets = df.apply(document_features, axis = 1)
 train_set, test_set = featuresets[250:], featuresets[:250]
-del(word_features)
 del(featuresets)
 
 classifier = nltk.NaiveBayesClassifier.train(train_set)
@@ -67,7 +67,7 @@ print(nltk.classify.accuracy(classifier, test_set))
 classifier.show_most_informative_features(20)
 
 from sklearn.svm import SVC
-svmClass = SklearnClassifier(SVC(C = .7)).train(train_set)
+svmClass = SklearnClassifier(SVC()).train(train_set)
 print("SVM Classifier:")
 print(nltk.classify.accuracy(svmClass, test_set))
 
@@ -80,3 +80,47 @@ from sklearn.neural_network import MLPClassifier
 nnClass = SklearnClassifier(MLPClassifier()).train(train_set)
 print("Neural Network Classifier:")
 print(nltk.classify.accuracy(nnClass, test_set))
+
+
+
+def svm_features(doc):
+    document_words = set(doc['reviewContent'])
+    features = {}
+    # Grabbing the bigrams
+    bigSet = []
+    for word in doc['bigrams']:
+        bigSet.append(word[0].lower() + " " +word[1].lower())
+    bigSet = set(bigSet)
+    for word in bigrams:
+        features['bigram: ' + word] = (word in bigSet)
+
+    # Counting the pronoun usage
+    meCount = 0
+    for word in doc['reviewContent']:
+        if (word.lower() == 'i' or word.lower() == 'me'):
+            meCount += 1
+    me = False
+    if (meCount > 5):
+        me = True
+
+    for word in word_features:
+        features['contains ' + word] = (word in document_words)
+    features.update(
+    {'rating': doc['rating'], 'useful': doc['usefulCount'], 'cool': doc['coolCount'], 'funny': doc['funnyCount'], 'meCount': me})
+    return features
+
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.model_selection import GridSearchCV
+parameters = {'kernel':('linear','poly', 'rbf'), 'C':[1, 10]}
+vec = DictVectorizer()
+trainTags, testTags = df[250:],df[:250]
+svmSet = df.apply(svm_features, axis = 1)
+svmSet = vec.fit_transform(svmSet).toarray()
+svmTest, svmTrain = svmSet[:250],svmSet[250:]
+
+                             
+svr = SVC()
+svmClass = GridSearchCV(svr, parameters)
+svmClass.fit(svmTrain,trainTags['tag'])
+print("SVM with Grid Search Cross-Validation: ")
+print(svmClass.score(svmTest,testTags['tag']))
